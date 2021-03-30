c3566e92b670390c1d6ea059a51404eafcfd511b
Asbjorn <heavymaskinen@hotmail.com>
2021-03-30 22:04:35 +0200

    Added new folders + Audio and IO projects from Core-branch.

---
+++ CoreSongReader/LibraryDeserializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Juke.IO;
+
+namespace CoreSongIO
+{
+    public interface LibraryDeserializer
+    {
+        List<PersistedSong> Deserialize(TextReader stream);
+    }
+}
---
+++ Messaging/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Juke.Control;
+
+namespace Juke.Log
+{
+    public class Logger
+    {
+        public static void ConsoleLog()
+        {
+            Messenger.LogMessagePosted += Messenger_LogMessagePosted;
+        }
+
+        private static void Messenger_LogMessagePosted(string message, Messenger.TargetType target)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
